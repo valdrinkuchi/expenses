a0626db0a3debd807df87f84b99de674ab0669b5
Valdrin Kuchi <valdrinkuchi@gmail.com>
2021-01-26 23:39:40 +0100

    Scrolling ability added

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,51 +8,55 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions);
 
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((el) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  ),
-                ),
-                padding: EdgeInsets.all(10),
-                child: Text('\$${el.amount}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple,
-                    )),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((el) {
+            return Card(
+              child: Row(
                 children: <Widget>[
-                  Text(
-                    el.title,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
                     ),
+                    padding: EdgeInsets.all(10),
+                    child: Text('\$${el.amount}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        )),
                   ),
-                  Text(
-                    DateFormat('dd/mm/yyyy').format(el.date),
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        el.title,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('dd/mm/yyyy').format(el.date),
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
                   )
                 ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
